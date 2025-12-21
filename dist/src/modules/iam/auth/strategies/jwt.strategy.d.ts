f8e228c95396785e35ee24301d064a3ec7cd0e74
13uraks <burak.sahin@pusula.com.tr>
2025-12-21 03:43:51 +0300

    dont know but need to push

--- dist/src/modules/iam/auth/strategies/jwt.strategy.d.ts
+++ dist/src/modules/iam/auth/strategies/jwt.strategy.d.ts
@@ -2,7 +2,7 @@ import { Strategy } from 'passport-jwt';
 import { ConfigService } from '@nestjs/config';
 import { PrismaService } from '../../../../core/prisma/prisma.service';
 import { JwtPayload, AuthenticatedUser } from '../../../../common/interfaces';
-declare const JwtStrategy_base: new (...args: [opt: import("passport-jwt").StrategyOptionsWithRequest] | [opt: import("passport-jwt").StrategyOptionsWithoutRequest]) => Strategy & {
+declare const JwtStrategy_base: new (...args: [opt: import("passport-jwt").StrategyOptionsWithoutRequest] | [opt: import("passport-jwt").StrategyOptionsWithRequest]) => Strategy & {
     validate(...args: any[]): unknown;
 };
 export declare class JwtStrategy extends JwtStrategy_base {
